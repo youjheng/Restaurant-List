ec4ca7b57ef51ac88f06ad74dae68af3d1118a71
youjheng <chagam0507@gmail.com>
2022-06-19 16:50:20 +0800

    initialize project

---
+++ README.md
@@ -0,0 +1,28 @@
+# Restaurant List 餐廳清單
+這是一個餐廳清單的模擬網站
+
+## Features
+* 使用者可以在首頁瀏覽所有餐廳的基本資訊
+* 使用者可以在首頁點選任一餐廳，進去看餐廳的詳細資訊
+* 使用者可以透過搜尋關鍵字(餐廳名稱或類別)來查詢特定餐廳
+
+## Getting Started
+
+### Prerequisites
+* node.js @14.16.0
+* express @4.16.4
+* express-handlebars @3.0.0
+* bootstrap @4.3.1
+
+### Installing
+1. 開啟終端機(Terminal)，clone 此專案至本機電腦
+```git clone ```
+2. cd 到存放專案本機位置並執行：
+```cd Restaurant List```
+3. 安裝 npm 套件：
+```npm install```
+4. 安裝 nodemon 套件
+```npm i nodemon```
+5. 啟動伺服器
+```npm run dev```
+6. 在瀏覽器輸入 http://localhost:3000 ，瀏覽網頁
---
+++ app.js
@@ -0,0 +1,37 @@
+// require packages
+const express = require('express')
+const app = express()
+const port = 3000
+const exphbs = require('express-handlebars')
+const restaurantList = require('./restaurant.json')
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting static files
+app.use(express.static('public'))
+
+// routes setting
+app.get('/', (req, res) => {
+  res.render('index', { restaurants: restaurantList.results })
+})
+
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  res.render('show', { restaurant: restaurant })
+})
+
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  const restaurants = restaurantList.results.filter(restaurant => {
+    return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || 
+      restaurant.category.toLowerCase().includes(keyword.toLowerCase())
+  })
+  res.render('index', { restaurants: restaurants, keyword: keyword })
+})
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
