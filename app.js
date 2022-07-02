f4975df53f24914a7cf10b9583dbdc962cb76e37
youjheng <chagam0507@gmail.com>
2022-07-02 18:24:37 +0800

    feat: update README.md

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use(bodyParser.urlencoded({ extended: true}))
 app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
+    .sort({ _id: 'asc' })
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.log(error))
 })
