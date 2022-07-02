c2b2e594edf851ad57c8d6ff0a08f32e404e05f1
youjheng <chagam0507@gmail.com>
2022-07-02 16:15:24 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -2,10 +2,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
-const Restaurant = require('./models/restaurant')
+const bodyParser = require('body-parser')
 
-const app = express()
-const port = 3000
+const Restaurant = require('./models/restaurant')
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -19,12 +18,13 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-// setting template engine
+const app = express()
+const port = 3000
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
-// setting static files
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true}))
 
 // routes setting
 app.get('/', (req, res) => {
@@ -34,21 +34,32 @@ app.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
-app.get('/restaurants/:id', (req, res) => {
-  const id = req.params.id
-  return Restaurant.findById(id)
-    .lean()
-    .then(restaurant => res.render('show', { restaurant }))
-    .catch(error => console.log(error))
-})
-
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = Restaurant.results.filter(restaurant => {
     return restaurant.name.toLowerCase().includes(keyword.toLowerCase()) || 
       restaurant.category.toLowerCase().includes(keyword.toLowerCase())
   })
-  res.render('index', { restaurants: restaurants, keyword: keyword })
+  res.render('index', { restaurants, keyword })
+})
+
+app.get('/restaurants/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/restaurants', (req, res) => {
+  const { name, name_en, category, image, location, phone, google_map, rating, description } =req.body
+  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
 })
 
 // start and listen on the Express server
