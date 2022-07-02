2d3ad2826b94fb6cae61305e1ef7651c25ca9dc6
youjheng <chagam0507@gmail.com>
2022-07-02 17:14:41 +0800

    feat: add delete todo function

--- app.js
+++ app.js
@@ -90,6 +90,14 @@ app.post('/restaurants/:id/edit', (req, res) => {
     .catch(error => console.log(error))
 })
 
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then(restaurant => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
 // start and listen on the Express server
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
