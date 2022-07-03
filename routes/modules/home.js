968271e8a1c3c0499578ed44d98c577680dea2ce
youjheng <chagam0507@gmail.com>
2022-07-03 10:26:54 +0800

    feat: add sorting function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,10 +4,11 @@ const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
 router.get('/', (req, res) => {
+  const sorting = req.query.sorting
   Restaurant.find()
     .lean()
-    .sort({ _id: 'asc' })
-    .then(restaurants => res.render('index', { restaurants }))
+    .sort({ name: sorting })
+    .then(restaurants => res.render('index', { restaurants, sorting }))
     .catch(error => console.log(error))
 })
 
