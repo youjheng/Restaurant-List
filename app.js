9cd68ed8c16f4acdd386f7094a68612913a6b765
youjheng <chagam0507@gmail.com>
2022-08-23 07:56:32 +0800

    feat: add message partials

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const flash = require('connect-flash')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -27,12 +28,15 @@ app.use(session({
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true}))
 app.use(methodOverride('_method'))
+app.use(flash())
 
 usePassport(app)
 
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
 
