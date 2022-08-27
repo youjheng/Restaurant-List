6101579e13052c03a9f858dfb7be69039e4cf975
youjheng <chagam0507@gmail.com>
2022-08-27 12:16:00 +0800

    feat: modify user login message

--- config/passport.js
+++ config/passport.js
@@ -8,15 +8,15 @@ module.exports = app => {
   app.use(passport.initialize())
   app.use(passport.session())
 
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+  passport.use(new LocalStrategy({ usernameField: 'email', passReqToCallback: true, }, (req, email, password, done) => {
     User.findOne({ email })
       .then(user => {
         if (!user) {
-          return done(null, false, { message: 'This email is not registered!' })
+          return done(null, false, req.flash('warning_msg', '這個Email尚未註冊！'))
         }
         return bcrypt.compare(password, user.password).then(isMatch => {
           if (!isMatch) {
-            return done(null, false, { message: 'Email or Password incorrect.' })
+            return done(null, false, req.flash('warning_msg', 'Email或密碼不正確。'))
           }
           return done(null, user)
         })
